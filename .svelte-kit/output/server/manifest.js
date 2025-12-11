aca0c443b0bec7d8b23439cfdb293fb426ca749d
Ricardo Chejfec <rchejfec.irpp@gmail.com>
2025-12-11 12:24:14 -0500

    Add CSV data files for static build, update gitignore

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["data/cd_metrics.csv","data/cd_metrics.json","data/cd_metrics.TEMPLATE_EXAMPLE.csv","data/cd_supplementary.csv","data/cd_supplementary.json","data/census_divisions.geojson"]),
 	mimeTypes: {".csv":"text/csv",".json":"application/json",".geojson":"application/geo+json"},
 	_: {
-		client: {start:"_app/immutable/entry/start.-UEhtv4F.js",app:"_app/immutable/entry/app.B8IuPwFn.js",imports:["_app/immutable/entry/start.-UEhtv4F.js","_app/immutable/chunks/DcJ-qL4k.js","_app/immutable/chunks/DnLhgM-Q.js","_app/immutable/chunks/CKX27AQ1.js","_app/immutable/entry/app.B8IuPwFn.js","_app/immutable/chunks/DnLhgM-Q.js","_app/immutable/chunks/BySWQMM0.js","_app/immutable/chunks/3TGghoft.js","_app/immutable/chunks/CKX27AQ1.js","_app/immutable/chunks/BowW3htO.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.CPdVMZqL.js",app:"_app/immutable/entry/app.DWxUB1LM.js",imports:["_app/immutable/entry/start.CPdVMZqL.js","_app/immutable/chunks/B3aFC_bH.js","_app/immutable/chunks/DnLhgM-Q.js","_app/immutable/chunks/CKX27AQ1.js","_app/immutable/entry/app.DWxUB1LM.js","_app/immutable/chunks/DnLhgM-Q.js","_app/immutable/chunks/BySWQMM0.js","_app/immutable/chunks/3TGghoft.js","_app/immutable/chunks/CKX27AQ1.js","_app/immutable/chunks/BowW3htO.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- .svelte-kit/output/server/nodes/1.js
+++ .svelte-kit/output/server/nodes/1.js
@@ -3,6 +3,6 @@
 export const index = 1;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.QsRnF4KQ.js","_app/immutable/chunks/3TGghoft.js","_app/immutable/chunks/DnLhgM-Q.js","_app/immutable/chunks/CRGCaXa7.js","_app/immutable/chunks/BySWQMM0.js","_app/immutable/chunks/Cuxx5man.js","_app/immutable/chunks/DcJ-qL4k.js","_app/immutable/chunks/CKX27AQ1.js"];
+export const imports = ["_app/immutable/nodes/1.DbaF-eSF.js","_app/immutable/chunks/3TGghoft.js","_app/immutable/chunks/DnLhgM-Q.js","_app/immutable/chunks/CRGCaXa7.js","_app/immutable/chunks/BySWQMM0.js","_app/immutable/chunks/Cuxx5man.js","_app/immutable/chunks/B3aFC_bH.js","_app/immutable/chunks/CKX27AQ1.js"];
 export const stylesheets = [];
 export const fonts = [];
